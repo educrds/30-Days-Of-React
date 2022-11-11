5bbdfa06bbc999f6dd3aa223b15d74b5bab37481
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-11-11 14:22:57 -0300

    Exercise Level 1 (Part 2) Complete

---
+++ solutions/day-01/level-1-part-2.js
@@ -0,0 +1,35 @@
+// 1
+let userAge = prompt("Enter your age: ");
+
+if (userAge >= 18) {
+  console.log("You are old enough to drive");
+} else {
+  console.log(`You are left with ${18 - userAge} years to drive.`);
+}
+
+// 2
+let yourAge = prompt("Enter your age: ");
+const myAge = 21;
+
+if (yourAge > myAge) {
+  console.log(`You are ${yourAge - myAge} years older than me.`);
+} else {
+  console.log(`You are ${myAge - yourAge} years younger than me`);
+}
+
+// 3
+let a = 4;
+let b = 3;
+
+if (a > b) {
+  console.log(`${a} is greater than ${b}`);
+} else {
+  console.log(`${a} is less than ${b}`);
+}
+
+// 4
+let yourNumber = prompt("Enter a number: ");
+
+yourNumber % 2 === 0
+  ? console.log(`${yourNumber} is an even number`)
+  : console.log(`${yourNumber} is an odd number`);
