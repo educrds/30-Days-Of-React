c7fd973d9880a0964bf40cdacd17f297e709a5ea
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-11-14 08:44:06 -0300

    Exercises Level 2 (Part 4) Complete

---
+++ solutions/day-01/level-2-part-4.js
@@ -0,0 +1,77 @@
+// 1
+function solveQuadEquation(a, b, c) {
+  const rPlus = ([-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)] / 2) * a;
+  const rMinus = ([-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)] / 2) * a;
+
+  return console.log(rPlus, rMinus);
+}
+
+// 2
+function printArray(arr) {
+  arr.map((item) => console.log(item));
+}
+
+// 3
+function showDateTime() {
+  const now = new Date();
+
+  return `${now.getDate()}/${
+    now.getMonth() + 1
+  }/${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}`;
+}
+
+// 4
+function swapValues(x, y) {
+  console.log(`x = ${y}, y = ${x}`);
+}
+
+// 5
+function reverseArray(array) {
+  return array.reverse();
+}
+
+// 6
+function capitalizeArray(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// 7
+function addItem(item) {
+  let array = [];
+  array.push(item);
+  return array;
+}
+
+// 9
+function evenAndOdds(number) {
+  let odd = 0;
+  let even = 0;
+
+  for (let i = 0; i <= number; i++) {
+    i % 2 === 0 ? even++ : odd++;
+  }
+  return console.log(
+    `The number of odds are ${odd}.\nThe number of evens are ${even}`
+  );
+}
+
+// 13
+function sumArguments() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
+
+// 1
+let guid = () => {
+  let s4 = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1)
+  };
+  return s4();
+};
+
+
