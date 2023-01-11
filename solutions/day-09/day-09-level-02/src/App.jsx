b24f21c6d49eb585ddca42c57dd2752571e40cb7
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2023-01-11 10:33:51 -0300

    Day 09 - Level 02 - 02 - Complete

--- solutions/day-09/day-09-level-02/src/App.jsx
+++ solutions/day-09/day-09-level-02/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Season from './Season';
+import Time from './Time';
+import './App.css';
 
 function App() {
-
   return (
-    <div className='App'>
-      <Season />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Season />}></Route>
+        <Route path='/02' element={<Time />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
