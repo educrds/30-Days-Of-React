d9bf66df5d4ccc3ae9c9abff0cd841521fce80e8
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2023-01-24 14:20:17 -0300

    Day 24 - Refactored Code #7

--- solutions/day-24/vite-project/src/components/CountriesList.jsx
+++ solutions/day-24/vite-project/src/components/CountriesList.jsx
@@ -1,4 +1,20 @@
-import CountryCard from './CountryCard';
+import Capital from './Capital';
+import Language from './Language';
+import Population from './Population';
+
+const CountryCard = ({ flags: { png }, name: { common }, capital, languages, population }) => {
+  return (
+    <div className='card'>
+      <img src={png} alt={common} />
+      <h1>{common}</h1>
+      <div className='infos'>
+        <Capital capital={capital} />
+        <Language languages={languages} />
+        <Population population={population} />
+      </div>
+    </div>
+  );
+};
 
 const CountriesList = ({ currentData }) => {
   return (
