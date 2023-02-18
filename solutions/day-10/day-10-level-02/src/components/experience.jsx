4e9b1797ac6b0abdf2d43bc228c2f645f2a516fc
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2023-02-18 13:07:51 -0300

    Day 10 - Level 02 - Add Certificate Links

--- solutions/day-10/day-10-level-02/src/components/experience.jsx
+++ solutions/day-10/day-10-level-02/src/components/experience.jsx
@@ -1,6 +1,6 @@
 import Container from './Container';
 import { SmallTitle, Title } from './TitleVariants';
-import { SiUdemy, SiUdacity, SiCoursera } from 'react-icons/si';
+import { TbCertificate } from 'react-icons/tb';
 import { useRef } from 'react';
 import { motion, useInView } from 'framer-motion';
 import { getTransformStyle } from '../utils';
@@ -9,33 +9,39 @@ import { Technologies } from './Projects';
 const experienceList = [
   {
     period: 'Janeiro - 2023',
+    title: 'Front-End JavaScript Junior',
+    resources: ['CSS', 'MySQL', 'HTML', 'JavaScript'],
+    link: 'https://i.postimg.cc/yx18k6TY/Eduardo-Cardoso-Barbosa.png',
+  },
+  {
+    period: 'Janeiro - 2023',
     title: 'The Complete Node.js Developer Course',
     resources: ['Node', 'MongoDB', 'SocketIO', 'Express', 'REST API'],
-    brand: <SiUdemy />,
+    link: 'https://udemy-certificate.s3.amazonaws.com/image/UC-f1ce6acc-4bb5-4791-ad44-4877e578d5a1.jpg?v=1672840221000',
   },
   {
     period: 'Novembro - 2022',
     title: 'Orange Tech +',
     resources: ['React', 'Typescript'],
-    brand: <SiUdacity />,
+    link: 'https://hermes.digitalinnovation.one/certificates/B1AC8419.pdf',
   },
   {
     period: 'Outubro - 2022',
     title: 'The Complete 2022 Web Development Bootcamp',
     resources: ['MySQL', 'React', 'MongoDB', 'Node', 'Bootstrap', 'Express'],
-    brand: <SiUdemy />,
+    link: 'https://udemy-certificate.s3.amazonaws.com/image/UC-1ad50437-35ce-4b4d-b4ae-4de6a852764b.jpg?v=1666789196000',
   },
   {
     period: 'Outubro - 2022',
     title: 'The Complete SQL Bootcamp 2022: Go from Zero to Hero',
     resources: ['MySQL', 'PostgreSQL'],
-    brand: <SiUdemy />,
+    link: 'https://udemy-certificate.s3.amazonaws.com/image/UC-2cf5b2c5-d13c-4fad-a3a2-2cb337b9e750.jpg?v=1664136644000',
   },
   {
     period: 'Outubro - 2022',
     title: 'Google UX Design',
-    resources: ['UI/UX designer', 'Interaction designer', 'Visual designer', 'Product designer'],
-    brand: <SiCoursera />,
+    resources: ['UI/UX designer', 'Interaction designer', 'Visual designer'],
+    link: '',
   },
 ];
 
@@ -62,7 +68,7 @@ const ExperienceList = () => {
         ))}
       </div>
       <div className='square'>
-        {experienceList.slice(-2).map((experience, i) => (
+        {experienceList.slice(-3).map((experience, i) => (
           <ExpInfo key={i} {...experience} />
         ))}
       </div>
@@ -70,16 +76,18 @@ const ExperienceList = () => {
   );
 };
 
-const ExpInfo = ({ period, title, resources, brand }) => {
+const ExpInfo = ({ period, title, resources, link }) => {
   return (
     <div className='column'>
       <div className='icon'>
-        {brand}
+        <TbCertificate />
         <div className='line'></div>
       </div>
       <div className='infos'>
         <SmallTitle content={period} />
-        <Title text={title} />
+        <a href={link} target='_blank'>
+          <Title text={title} />
+        </a>
         <Technologies techList={resources} />
       </div>
     </div>
